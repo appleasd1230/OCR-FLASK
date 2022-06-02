2903d47305752dc32268ce54f79b1ec768d6c4d1
Josh1994 <appleasd1230@gms.tku.edu.tw>
2022-06-02 15:13:59 +0800

    Update config.py

--- service/app/config/config.py
+++ service/app/config/config.py
@@ -1,1 +1,19 @@
+# -*- coding:utf-8 -*-
+import os
+import datetime
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class BaseConfig:  # 基本配置
+    PERMANENT_SESSION_LIFETIME = datetime.timedelta(days=14)
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = False
+
+class TestingConfig(BaseConfig):
+    TESTING = True
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+}
