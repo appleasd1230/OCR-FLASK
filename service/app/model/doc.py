bfc4958ebc4b29a03b9617f362f83f024f583518
Josh1994 <appleasd1230@gms.tku.edu.tw>
2022-06-02 15:15:17 +0800

    Update doc.py

--- service/app/model/doc.py
+++ service/app/model/doc.py
@@ -1,1 +1,17 @@
+# -*- coding:utf-8 -*-
 
+"""辨識結果class"""
+class RecResultModel(object):
+    def __init__(self):
+        self.status = '' # 結果 0:處理中 / 1:完成 / -1:異常
+        self.msg = '' # 如果異常紀錄訊息
+        self.result = [] # 文件類型
+
+"""公文辨識結果class"""
+class DepositoryDocModel(object):
+    def __init__(self):
+        self.documentType = '公文' # 文件類型
+        self.documentName = '' # 檔案名稱
+        self.status = '0' # 狀態 0:處理中 / 1:完成 / -1:異常
+        self.msg = '' # 如果異常紀錄訊息
+        self.value = [] # 欄位and值
